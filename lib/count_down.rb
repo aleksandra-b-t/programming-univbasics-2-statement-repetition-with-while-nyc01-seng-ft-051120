182d123a73e4fb235709e4ccd0ee53c688e07d0a
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-25 14:02:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,7 @@
 # Write your code here
 count = 10
-while count > 0
-do  puts count
-break if count == 0
-puts "Happy New Year !"
+while count >= 1 do  
+  puts count
 count -= 1
-end
+end 
+puts "Happy New Year !"
